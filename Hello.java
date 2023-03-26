7060ad84ca3ddf130d93629a29225adef186180f
张欧文 <1534661514@qq.com>
2023-03-26 15:38:32 +0800

    update basic

--- Hello.java
+++ Hello.java
@@ -1,7 +1,28 @@
+// java快速入门
+// 对代码的相关说明
+
+// 1. public class Hello表示Hello是一个类,是一个public公有的类
+// 2. Hello {} 表示一个类的开始与结束
+// 3. public static void main(String[] args) 表示一个主方法,即我们程序的入口
+// 4. main() {}表示方法的开始与结束
+// 5. System.out.println(“hello,world”) 表示输出
+// 6. ; 表示语句的结束
 public class Hello {
 
     // 编写一个主方法
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("小欧宝 is studing \t java");
     }
+}
+
+class Dog{
+    public static void main(String[] args) {
+        System.out.println("hello, dog");
+    }
+}
+class Cat{
+    public static void main(String[] args) {
+        System.out.println("hello, cat");
+    }
+
 }
