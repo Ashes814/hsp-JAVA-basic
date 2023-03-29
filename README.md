b9db48486126fca37a12a6a1f89ccf7471161fcd
张欧文 <1534661514@qq.com>
2023-03-29 15:43:05 +0800

    auto data type change

--- README.md
+++
@@ -1,2 +0,0 @@
-# hsp-JAVA-basic
-Code and File of 韩顺平JAVA基础
--- chapter3/demo/Test.java
+++ chapter3/demo/Test.java
@@ -1,9 +1,15 @@
 public class Test {
     public static void main(String[] args) {
-        double n3 = 8848.88888f;
-        double n4 = 8848.88888;
-        System.out.println(n3);
-        System.out.println(n4);
+        int n1 = 10;
+        double d1 = n1 + 1.1; //(1.1为double类型,n1为int类型,计算时自动转换成double,double向float赋值会报错)
+        char c1 = 'b';
+        double c2 = c1;
+
+     
+        System.out.println(n1);
+        System.out.println(d1);
+        System.out.println(c2);
+       
 
     }
 }
