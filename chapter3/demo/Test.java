a729ec4d91654709db433e8f4389088e9746a43b
张欧文 <1534661514@qq.com>
2023-03-30 14:25:04 +0800

    homework c3

--- chapter3/demo/Test.java
+++ chapter3/demo/Test.java
@@ -1,14 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        int n1 = 10;
-        double d1 = n1 + 1.1; //(1.1为double类型,n1为int类型,计算时自动转换成double,double向float赋值会报错)
-        char c1 = 'b';
-        double c2 = c1;
+       String s1 = "88482";
+       int s2 = Integer.parseInt(s1);
+       System.out.println(s2);
 
-     
-        System.out.println(n1);
-        System.out.println(d1);
-        System.out.println(c2);
+
        
 
     }
---
+++ chapter3/homework/Homework04.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class Homework04 {
+    public static void main(String[] args) {
+        String name = "古娜拉";
+        int age = 8848;
+        double score = 88.88;
+        char gender = 'F';
+        String hobby = "黑魔法";
+
+        // 输出
+        System.out.println("姓名\t年龄\t分数\t性别\t爱好\n" + 
+                            name + "\t" + age + "\t" + 
+                            score + "\t" + gender + "\t" + 
+                            hobby);
+
+    }
+}
