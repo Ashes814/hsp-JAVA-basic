6d77387754071ba85d2047fc765b3b8728d2cbd3
张欧文 <1534661514@qq.com>
2023-04-02 14:06:45 +0800

    update homework c4

--- chapter4/homework/Homework01.java
+++ chapter4/homework/Homework01.java
@@ -2,15 +2,10 @@ package homework;
 
 public class Homework01 {
     public static void main(String [] args) {
-        int n1;
-        n1 = 13;
-        int n2;
-        n2 = 17;
-        int n3;
-        n3 = n1 + n2; // 30
-        System.out.println("n3 = " + n3); // n3 = 30
-        int n4 = 38;
-        int n5 = n4 - n3;
-        System.out.println("n5 = " + n5); // n5 = 8
+        System.out.println(10/3);//3
+        System.out.println(10/5);//2
+        System.out.println(10%2);//0
+        System.out.println(-10.5%3);//-1.5
+
     }
 }
