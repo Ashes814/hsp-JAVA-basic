62e14d0e91dff7d4fccbda3f5673e2063dcdcdee
张欧文 <1534661514@qq.com>
2023-04-04 16:15:40 +0800

    update branch

--- chapter5/demo/Test.java
+++ chapter5/demo/Test.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-       int a = 1 >> 2; //0 00000000 00000000 00000000 00000001
-       int b = -1 >> 2; //-1 111111111 11111111 11111111 11111110
-       int c = 1 << 2; //4 00000000 00000000 00000000 00000100
-       int d = -1 << 2; //-4 100000000 00000000 00000000 00000100
-       int e = -1 >>> 3; //536870911 000111111 11111111 11111111 11111111
 
        System.out.println(a);
        System.out.println(b);
