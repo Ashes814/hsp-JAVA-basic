550f6a8e761ef2796bb0d025d32bc098b887c106
张欧文 <1534661514@qq.com>
2023-04-08 20:12:16 +0800

    update hole pyramid

--- chapter5/demo/Test.java
+++ chapter5/demo/Test.java
@@ -8,15 +8,31 @@ public class Test {
         //     System.out.println(9);
         // }
 
-    Scanner input = new Scanner(System.in);
-    int level = input.nextInt();
+    // Scanner input = new Scanner(System.in);
+    // int level = input.nextInt();
+    int level = 7;
     int i = 1;
     while(i <= level) {
-        for(int j = 1;j<=i;j++) {
+        for(int k = 1;k<=(level - i);k++) {
             System.out.print(" ");
-            System.out.print('*');
+            // System.out.print(" ");
             
         }
+
+
+        for(int j = 1;j<=i;j++) {
+
+            if (j == 1 | j == i | i == level) {
+                System.out.print('*');
+            } else {
+                System.out.print(' ');
+            }
+            
+            System.out.print(" ");
+            
+        }
+
+       
         System.out.print('\n');
         i++;
     }
