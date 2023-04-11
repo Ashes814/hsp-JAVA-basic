b579a3e344993b362e1e3a42d5e7e229950f8adc
张欧文 <1534661514@qq.com>
2023-04-11 15:49:24 +0800

    oop method details

--- chapter7/demo/Test.java
+++ chapter7/demo/Test.java
@@ -2,21 +2,85 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String inputName = scanner.next();
-        switch (inputName) {
-            case "小白":
-                System.out.println("小白,绿色,3");
-                break;
-            case "小花":
-                System.out.println("小花,花色,8848");
-                break;
-            default:
-                System.out.println("小猫咪离家出走");
-        }
+        
+        // Cat cat1 = new Cat();
+        // Cat cat2 = new Cat();
 
-        scanner.close();
+
+        // cat1.name = "little white";
+        // cat2.name = "little colorful";
+
+        // cat1.age = 3;
+        // cat2.age = 100;
+
+
+        // cat1.color = "white";
+
+        // cat2.color = "colorful";
+
+        // cat1.weight = 12;
+        // cat2.weight = 80;
+
+        // System.out.println(cat1.name + cat1.age + cat1.color + cat1.weight);
+        // System.out.println(cat2.name + cat2.age + cat2.color + cat2.weight);
+        // Person p1 = new Person();
+        // p1.age = 10;
+        // p1.name = "ming";
+
+        // Person p2 = p1;
+        // p1.age = 80;
+        // System.out.println(p2.age);
+        // System.out.println(p1.age);
+        // Person p1 = new Person();
+        // p1.speak();
+        // p1.cal01();
+        // p1.cal02(100);
+        // System.out.println(p1.getSum(1,2));
+        Person p1 = new Person()
+        p1.map = {{1,2,3}, {4,5,6}, {7,8,9}};
+
 
     }
        
+
+
 }
+
+
+class Person {
+    String name;
+    int age;
+    int map[][];
+
+
+    // public void speak() {
+    //     System.out.println("I am your father");
+    // }
+
+    // public void cal01() {
+    //     int sum = 0;
+    //     for (int i = 1; i <= 1000; i++){
+    //         sum += i;
+    //     }
+    //     System.out.println(sum);
+    // }
+
+    // public void cal02(int n) {
+    //     int sum = 0;
+    //     for (int i = 1; i <= n; i++){
+    //         sum += i;
+    //     }
+    //     System.out.println(sum);
+    // }
+
+    // public int getSum(int num1, int num2) {
+    //     int res = num1 + num2;
+    //     return res;
+    // }
+
+    public void printArray() {
+        
+    }
+
+}
+
