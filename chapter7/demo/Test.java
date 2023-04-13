f099e54638364a6289e8d324d82c9e3600093907
张欧文 <1534661514@qq.com>
2023-04-13 13:22:31 +0800

    clone object

--- chapter7/demo/Test.java
+++ chapter7/demo/Test.java
@@ -2,43 +2,30 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        
-        // Cat cat1 = new Cat();
-        // Cat cat2 = new Cat();
+        int[][] testArray = {{1},{1,2},{1,2,3}};
+        MyTool t1 = new MyTool();
+        t1.print2D(testArray);
+
+        Person p1 = new Person();
+        p1.name = "oo";
+        p1.age = 8848;
+
+        Person p2 = p1;
+        Person newP1 = t1.copyPerson(p1);
 
 
-        // cat1.name = "little white";
-        // cat2.name = "little colorful";
+        p2.age = 9949;
 
-        // cat1.age = 3;
-        // cat2.age = 100;
+        newP1.age = 55;
+        p1.age = 15;
 
+        p1.name = "jj";
+        System.out.println("p1:" + p1.name + p1.age);
+        System.out.println("p2:" + p2.name + p2.age);
+        System.out.println("newP1:" + newP1.name + newP1.age);
 
-        // cat1.color = "white";
-
-        // cat2.color = "colorful";
-
-        // cat1.weight = 12;
-        // cat2.weight = 80;
-
-        // System.out.println(cat1.name + cat1.age + cat1.color + cat1.weight);
-        // System.out.println(cat2.name + cat2.age + cat2.color + cat2.weight);
-        // Person p1 = new Person();
-        // p1.age = 10;
-        // p1.name = "ming";
-
-        // Person p2 = p1;
-        // p1.age = 80;
-        // System.out.println(p2.age);
-        // System.out.println(p1.age);
-        // Person p1 = new Person();
-        // p1.speak();
-        // p1.cal01();
-        // p1.cal02(100);
-        // System.out.println(p1.getSum(1,2));
-        Person p1 = new Person()
-        p1.map = {{1,2,3}, {4,5,6}, {7,8,9}};
-
+        System.out.println(p1.equals(newP1));
+        System.out.println(p1.equals(p2));
 
     }
        
@@ -46,41 +33,36 @@ public class Test {
 
 }
 
-
 class Person {
     String name;
     int age;
-    int map[][];
 
 
-    // public void speak() {
-    //     System.out.println("I am your father");
-    // }
+}
 
-    // public void cal01() {
-    //     int sum = 0;
-    //     for (int i = 1; i <= 1000; i++){
-    //         sum += i;
-    //     }
-    //     System.out.println(sum);
-    // }
 
-    // public void cal02(int n) {
-    //     int sum = 0;
-    //     for (int i = 1; i <= n; i++){
-    //         sum += i;
-    //     }
-    //     System.out.println(sum);
-    // }
+class MyTool {
+    
 
-    // public int getSum(int num1, int num2) {
-    //     int res = num1 + num2;
-    //     return res;
-    // }
+    public void print2D(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+  
+    public Person copyPerson(Person personObject) {
 
-    public void printArray() {
-        
+        Person newObject = new Person();
+        newObject.name = personObject.name;
+        newObject.age = personObject.age;
+
+        return newObject;
+
     }
 
 }
+
 
