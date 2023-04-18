6509a3cbeade6ac03466495ca716f45c8274c43f
Ashes814 <1534661514@qq.com>
2023-04-18 19:33:22 +0800

    update java method override

---
+++ chapter8/src/com/pack/override_/Animal.java
@@ -0,0 +1,9 @@
+package com.pack.override_;
+
+public class Animal {
+    protected void soak() {
+        System.out.println("yarn!!!!!");
+    }
+}
+
+
---
+++ chapter8/src/com/pack/override_/Dog.java
@@ -0,0 +1,10 @@
+package com.pack.override_;
+
+public class Dog extends Animal {
+    public void soak() {
+        System.out.println("WOOF!");
+    }
+//    public void cry() {
+//        System.out.println("WOOF!");
+//    }
+}
