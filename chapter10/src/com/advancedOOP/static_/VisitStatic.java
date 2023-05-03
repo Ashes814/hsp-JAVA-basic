b45d6d227096a0fce09fd5375c85dc22cdcb07ee
Ashes814 <1534661514@qq.com>
2023-05-03 14:38:12 +0800

    static method basic

---
+++ chapter10/src/com/advancedOOP/static_/VisitStatic.java
@@ -0,0 +1,13 @@
+package com.advancedOOP.static_;
+
+public class VisitStatic {
+    public static void main(String[] args) {
+        System.out.println(A.name);
+
+    }
+}
+
+
+class A {
+    public static String name = "your mother BOOM!";
+}
