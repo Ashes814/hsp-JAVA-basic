f9a380437ff796a15d0a6b81167f4730a793d0d0
Ashes814 <1534661514@qq.com>
2023-05-04 19:04:17 +0800

    abstract class start

---
+++ chapter10/src/com/advancedOOP/abstract_/Abstract01.java
@@ -0,0 +1,35 @@
+package com.advancedOOP.abstract_;
+
+public class Abstract01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    abstract public void work();
+
+    public String getName() {
+        return name;
+    }
+}
+
+class CommonEmployee extends Employee {
+    public CommonEmployee(String name, int id, double salary) {
+        super(name, id, salary);
+    }
+
+    public void work() {
+        System.out.println(this.getName() + "is Common is working");
+    }
+}
