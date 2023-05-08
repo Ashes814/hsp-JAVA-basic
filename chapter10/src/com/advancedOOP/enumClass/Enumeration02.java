544c1594a28661db0a6cdf3cbdf8d9e87290e0d4
Ashes814 <1534661514@qq.com>
2023-05-08 18:12:30 +0800

    constructor without parameter

--- chapter10/src/com/advancedOOP/enumClass/Enumeration02.java
+++ chapter10/src/com/advancedOOP/enumClass/Enumeration02.java
@@ -5,4 +5,50 @@ package com.advancedOOP.enumClass;
  * @version 1.0
  */
 public class Enumeration02 {
+    public static void main(String[] args) {
+        System.out.println(Season.AUTUMN);
+
+
+    }
+}
+
+enum SeasonEnum {
+    SPRING("Spring","Warm"),
+    SUMMER("SUMMER","Hot"),
+    AUTUMN("AUTUMN","Cool"),
+    WHAT,
+    WINTER("WINTER","Cold");
+
+    private String name;
+    private String desc;
+
+    private SeasonEnum(){
+        System.out.println("no");
+    }
+
+//    public static final Season SPRING = new Season("Spring","Warm");
+//    public static final Season SUMMER = new Season("SUMMER","Hot");
+//    public static final Season AUTUMN = new Season("AUTUMN","Cool");
+//    public static final Season WINTER = new Season("WINTER","Cold");
+
+
+
+    private SeasonEnum(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.getName() + this.getDesc();
+    }
 }
