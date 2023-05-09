e3dc8ea0f8c67aa90e22b7ca093484cb4afc5fad
Ashes814 <1534661514@qq.com>
2023-05-09 12:12:11 +0800

    finish homework02

--- chapter10/src/com/advancedOOP/homework/Homework02.java
+++ chapter10/src/com/advancedOOP/homework/Homework02.java
@@ -5,4 +5,35 @@ package com.advancedOOP.homework;
  * @version 1.0
  */
 public class Homework02 {
+    public static void main(String[] args) {
+        System.out.println(Frock.getNextNum());
+        System.out.println(Frock.getNextNum());
+
+        Frock frock1 = new Frock();
+        Frock frock2 = new Frock();
+        Frock frock3 = new Frock();
+
+        System.out.println(frock1.getSerialNumber());
+        System.out.println(frock2.getSerialNumber());
+        System.out.println(frock3.getSerialNumber());
+
+    }
+}
+
+class Frock {
+    private static int currentNum = 100000;
+    public static int getNextNum() {
+        currentNum += 100;
+        return currentNum;
+    }
+
+    private int serialNumber;
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public Frock() {
+        this.serialNumber = getNextNum();
+    }
 }
