0f526eee25ad927fb2cdd02a9452febee0aae7de
Ashes814 <1534661514@qq.com>
2023-05-10 12:34:40 +0800

    ArrayIndexOutOfBoundException

---
+++ chapter12/src/com/exception_/basic/Exception01.java
@@ -0,0 +1,15 @@
+package com.exception_.basic;
+
+/**
+ * @author 欧欧
+ * @version 1.0
+ */
+public class Exception01 {
+    public static void main(String[] args) {
+       String[] arr = {"1", "@"};
+
+        System.out.println(arr[2]);
+
+
+    }
+}
