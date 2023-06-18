630097086c5842a256383c85b5f52d8ec2b780f2
Ashes814 <1534661514@qq.com>
2023-06-18 21:33:50 +0800

    first socket program

--- chapter21_network/src/com/netProgramming/SocketTCP02Client.java
+++ chapter21_network/src/com/netProgramming/SocketTCP02Client.java
@@ -1,6 +1,7 @@
 package com.netProgramming;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -9,15 +10,20 @@ import java.net.Socket;
  * @author 欧欧
  * @version 1.0
  */
-public class SocketTCP01Client {
+public class SocketTCP02Client {
     public static void main(String[] args) throws IOException {
-        // 1. connect server,(ip, port)
         Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
-        System.out.println("client" + socket.getClass());
-        // 2. generate Socket ny socket.getOutputStream()
         OutputStream outputStream = socket.getOutputStream();
+        InputStream inputStream = socket.getInputStream();
         // 3. write data to socket by output stream
         outputStream.write("hello, server".getBytes());
+
+        byte[] buf = new byte[1024];
+        int readLen = 0;
+        while ((readLen = inputStream.read(buf)) != -1) {
+            System.out.println(new String(buf, 0,readLen));
+        }
+
         // 4. close stream object and socket
         outputStream.close();
         socket.close();
--- chapter21_network/src/com/netProgramming/SocketTCP02Server.java
+++ chapter21_network/src/com/netProgramming/SocketTCP02Server.java
@@ -3,6 +3,7 @@ package com.netProgramming;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,26 +11,25 @@ import java.net.Socket;
  * @author 欧欧
  * @version 1.0
  */
-public class SocketTCP01Server {
+public class SocketTCP02Server {
     public static void main(String[] args) throws IOException {
-        // 1. listening 9999 port, waiting for connection
-        ServerSocket serverSocket = new ServerSocket(9999); // 9999 should not be used by other program
-        System.out.println("Server listening for 9999, waiting for connections...");
-        // 2. if no client connects to 9999, block
+        ServerSocket serverSocket = new ServerSocket(9999);
         Socket socket = serverSocket.accept();
-        System.out.println(socket.getClass());
 
-        // 3. Reading and showing data by socket.getInputStream
         InputStream inputStream = socket.getInputStream();
-        // 4. IO read
+        OutputStream outputStream = socket.getOutputStream();
         byte[] buf = new byte[1024];
         int readLen = 0;
         while ((readLen = inputStream.read(buf)) != -1) {
-            System.out.println(new String(buf, 0, readLen));
+            System.out.println(new String(buf, 0,readLen));
         }
-        // 5. close
+        outputStream.write("hello, client".getBytes());
+
+
+        outputStream.close();
         inputStream.close();
         socket.close();
         serverSocket.close();
+
     }
 }
