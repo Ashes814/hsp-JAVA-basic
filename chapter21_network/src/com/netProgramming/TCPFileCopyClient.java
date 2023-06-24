bba1c7f96a65d53b297acc54258bb4e511723ed2
Ashes814 <1534661514@qq.com>
2023-06-24 16:43:13 +0800

    update copy socket

--- chapter21_network/src/com/netProgramming/TCPFileCopyClient.java
+++ chapter21_network/src/com/netProgramming/TCPFileCopyClient.java
@@ -1,11 +1,42 @@
 package com.netProgramming;
 
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * @author 欧欧
  * @version 1.0
  */
 public class TCPFileCopyClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // 客户端连接服务端，得到Socket对象
+        Socket socket = new Socket(InetAddress.getLocalHost(), 8888);
+        String filePath = "/Users/zowcool/Desktop/Learning/hsp-JAVA-basic/chapter21_network/out/qie.jpg";
+
+        // 创建读取磁盘文件的输入流
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filePath));
+
+
+        // bytes就是file对应的字符数组
+        byte[] bytes = StreamUtils.streamToByteArray(bis);
+
+        //通过socket获取到输出流，将bytes数据发送给服务端
+        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+        bos.write(bytes); //将文件对应的字节数组的内容，写入到数据通道
+        bis.close();
+        socket.shutdownOutput(); //设置结束标志
+
+
+        // get server message
+        InputStream inputStream = socket.getInputStream();
+        String s = StreamUtils.streamToString(inputStream);
+        System.out.println(s);
+
+inputStream.close();
+        //关闭相关流
+        bos.close();
+        socket.close();
 
     }
 }
--- chapter21_network/src/com/netProgramming/TCPFileCopyServer.java
+++ chapter21_network/src/com/netProgramming/TCPFileCopyServer.java
@@ -1,11 +1,44 @@
 package com.netProgramming;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author 欧欧
  * @version 1.0
  */
 public class TCPFileCopyServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //1. 服务端在本机监听8888端口
+        ServerSocket serverSocket = new ServerSocket(8888);
+        System.out.println("Server is listening 8888");
+
+        //2.等待连接
+        Socket socket = serverSocket.accept();
+
+        //3.读取客户端发送的数据
+        // 通过Socket得到输入
+        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+        byte[] bytes = StreamUtils.streamToByteArray(bis);
+        //4. 将得到bytes数组，写入到指定的路径，就得到一个文件了
+        String destFilePath = "/Users/zowcool/Desktop/Learning/hsp-JAVA-basic/chapter21_network/out/qie2.jpg";
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(destFilePath));
+        bos.write(bytes);
+        bos.close();
+
+        //向客户端回复收到图片
+        // 通过socket获取到输出流
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        writer.write("收到图片");
+        writer.flush(); // 把内容刷新到数据通道
+        socket.shutdownOutput();
+        writer.close();
+
+
+        bis.close();
+        socket.close();
+        serverSocket.close();
 
     }
 }
